2466751b204c2d0ca4ed9bcfaaefdae16544d4b1
Tungdil83 <129401421+Tungdil83@users.noreply.github.com>
2026-09-01 13:28:52 +0200

    moved SitemapService.java

--- backend/src/main/java/io/papermc/hangar/components/sitemap/SitemapService.java
+++ backend/src/main/java/io/papermc/hangar/components/sitemap/SitemapService.java
@@ -66,16 +66,14 @@ public class SitemapService extends HangarComponent {
     private static SitemapGenerator addGlobal(SitemapGenerator generator) {
         generator
             .addPage(WebPage.builder().name("").changeFreqDaily().priority(1.0).build())
-            .addPage(WebPage.builder().name("paper").changeFreqDaily().priority(1.0).build())
-            .addPage(WebPage.builder().name("waterfall").changeFreqDaily().priority(1.0).build())
-            .addPage(WebPage.builder().name("velocity").changeFreqDaily().priority(1.0).build())
+            .addPage(WebPage.builder().name("side_plugins").changeFreqDaily().priority(1.0).build())
+            .addPage(WebPage.builder().name("official").changeFreqDaily().priority(1.0).build())
+            .addPage(WebPage.builder().name("abilities").changeFreqDaily().priority(1.0).build())
+            .addPage(WebPage.builder().name("search").changeFreqDaily().priority(1.0).build())
             .addPage(WebPage.builder().name("authors").changeFreqWeekly().priority(1.0).build())
             .addPage(WebPage.builder().name("staff").changeFreqWeekly().priority(1.0).build())
             .addPage(WebPage.builder().name("guidelines").changeFreqMonthly().priority(1.0).build())
-            .addPage(WebPage.builder().name("terms").changeFreqMonthly().priority(1.0).build())
-            .addPage(WebPage.builder().name("privacy").changeFreqMonthly().priority(1.0).build())
-            .addPage(WebPage.builder().name("version").changeFreqWeekly().priority(1.0).build())
-            .addPage(WebPage.builder().name("api-docs").changeFreqMonthly().priority(1.0).build());
+            .addPage(WebPage.builder().name("terms").changeFreqMonthly().priority(1.0).build());
         return generator;
     }
 
